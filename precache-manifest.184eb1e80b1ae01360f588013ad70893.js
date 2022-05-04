c56bcea66598f6f997d502f8dc7a55b16446c1c9
kevin-dignizant <kevin-dignizant@users.noreply.github.com>
2022-05-04 10:10:33 +0000

    deploy: 7ca14fd99991ffed88cf352c05e09ddc3b225540

---
+++ precache-manifest.184eb1e80b1ae01360f588013ad70893.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67a78084217c1e5cc283f1ad1a08b6b0",
+    "url": "https://kevin-dignizant.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "63b64b6be40d3fe74ff3",
+    "url": "https://kevin-dignizant.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "90df827dd6acc572d12f",
+    "url": "https://kevin-dignizant.github.io/task-management-frontend/static/js/2.2a95e268.chunk.js"
+  },
+  {
+    "revision": "63b64b6be40d3fe74ff3",
+    "url": "https://kevin-dignizant.github.io/task-management-frontend/static/js/main.a0e059a2.chunk.js"
+  },
+  {
+    "revision": "a2ee654cc53d7aa207d7",
+    "url": "https://kevin-dignizant.github.io/task-management-frontend/static/js/runtime~main.cd275be0.js"
+  }
+]);
